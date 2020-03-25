3f6abeff692faeb4913feccc1e3efe675e5e7c40
Florent <florent.chavanne@econocom.com>
2020-03-25 12:56:29 +0100

    optimisation : get translations from main query

--- src/Controller/defaultController.php
+++ src/Controller/defaultController.php
@@ -189,10 +189,12 @@ class defaultController extends AbstractController
                 'class' => Equipment::class,
                 'query_builder' => function (EntityRepository $er) use ( $defaultData ) {
                     return $er->createQueryBuilder('u')
-                        ->select('u', 'gs')
+                        ->select('u', 'gs', 't')
                         ->leftJoin('u.gearSet', 'gs')
+                        ->leftJoin('u.translations', 't', 'WITH', 't.locale = :locale')
                         ->andWhere('gs.gameVersion = :lastVersion')
                         ->setParameter('lastVersion', $defaultData['version'])
+                        ->setParameter('locale', $this->locale)
                         ->orderBy('u.gearSet', 'ASC');
                 },
                 'choice_label' => function (Equipment $equipment) {
