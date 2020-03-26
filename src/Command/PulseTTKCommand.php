e40b636f12463d04c05bbdb1f806e27781bdddc3
Florent <florent.chavanne@econocom.com>
2020-03-26 10:02:46 +0100

    few optimisation and code arrange. Add pulse

---
+++ src/Command/PulseTTKCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PulseTTKCommand extends Command
+{
+    protected static $defaultName = 'app:pulseTTK';
+    private $gameVersionService;
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Import last game version from survarim.pro.')
+            ->setHelp('no option');
+    }
+
+    public function __construct(GameVersionService $gameVersionService)
+    {
+        $this->gameVersionService = $gameVersionService;
+        parent::__construct();
+    }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $url = "https://pi4.freeboxos.fr/survarium";
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        $curl_output = curl_exec($curl);
+        curl_close($curl);
+        $date = \DateTime::ATOM;
+        $output->writeln("Warming ".$url." cache ( ".$date." ).");
+        return 0;
+    }
+}
